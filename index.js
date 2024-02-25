10385f63b079a4bd8b9778d99e508f7cb46b9934
tvymola <156726450+tvymola@users.noreply.github.com>
2024-02-25 14:50:27 -0600

    Modify the function to eliminate errors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// Define a function that prompts the user for their name, then greets the user by name
+// Import the prompt-sync package
+const prompt = require("prompt-sync")({ sigint: true });
+
+/* Define a function that prompts the user for their name, then greets the user by name. Use the prompt-sync package to get user input. */
+
 function greet() {
-  let name = prompt("What's your name?");
-  return name;
+  const userName = prompt("What's your name?");
+  // Print the result to the console greeting the user
+  console.log(`Hello, ${userName}!`);
 }
 
-// Call the function to pass the name value to the console log
-const userName = greet();
-
-// Print the result to the console greeting the user
-console.log("Hello, ", userName);
+// Call the function to greet the user
+greet();
