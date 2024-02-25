42c82f865a33cda78dcf7739dfb9970e9971dd9d
tvymola <156726450+tvymola@users.noreply.github.com>
2024-02-25 16:54:41 -0600

    Call the function and pass in my name as a string argument

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 function greet(name) {
   return `Hello, ${name}`;
 }
+
+// Call the function passing in my name as a string argument and assigning the return value to a variable called greeting
+const greeting = greet("Tracy");
