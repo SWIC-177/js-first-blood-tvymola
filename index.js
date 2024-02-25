7104c27d2ebae5ed3dd30fd652e95258af4967a6
tvymola <156726450+tvymola@users.noreply.github.com>
2024-02-25 13:52:06 -0600

    Modify logic for function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Define a function that prompts the user for their name, then greets the user by name
 function greet() {
-  let username = window.prompt("What is your name?");
-  return username;
+  let name = prompt("What's your name?");
+  return name;
 }
 
 // Call the function to pass the name value to the console log
-const user = greet();
+const userName = greet();
 
 // Print the result to the console greeting the user
-console.log("Hello, ", user);
+console.log("Hello, ", userName);
