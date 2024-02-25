e0f72f1051a5c75a2ed8b8ef412f7076c1425f63
tvymola <156726450+tvymola@users.noreply.github.com>
2024-02-25 13:31:59 -0600

    Correct logic in function contained in index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+// Define a function that prompts the user for their name, then greets the user by name
 function greet() {
-  let name = window.prompt("What's your name?");
-  console.log("Hello, ", name);
+  let username = window.prompt("What is your name?");
+  return username;
 }
 
-greet();
+// Call the function to pass the name value to the console log
+const user = greet();
+
+// Print the result to the console greeting the user
+console.log("Hello, ", user);
