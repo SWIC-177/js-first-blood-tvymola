add73be1317c317fb3093b1084e3bcbc691a0d46
tvymola <156726450+tvymola@users.noreply.github.com>
2024-03-22 18:37:55 -0500

    Add a function verifyAdulthood to the project

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-/* eslint-disable no-console */
-// Function definition for a process that greets the user by name
-function greet(name) {
-  return `Hello, ${name}`;
+// Declare variables for the user's name and age
+const name = "Tracy";
+const age = 16;
+
+/* Function definition for a process that uses an IF/ELSE statement that verifies the user's age prior to allowing entry */
+function verifyAdulthood(x, y) {
+  if (y >= 18) {
+    console.log(`Welcome, ${x}!`);
+  } else {
+    console.log(`You are not old enough to enter, ${x}.`);
+  }
 }
-
-/* Call the function passing in my name as a string argument and assigning the return value to a variable called greeting */
-const greeting = greet("Tracy");
-
-// Print the result to the console greeting the user
-console.log(greeting);
+// Call the function passing in the users name and age as arguments
+verifyAdulthood(name, age);
---
+++ index2.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+// Function definition for a process that greets the user by name
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+/* Call the function passing in my name as a string argument and assigning the return value to a variable called greeting */
+const greeting = greet("Tracy");
+
+// Print the result to the console greeting the user
+console.log(greeting);
