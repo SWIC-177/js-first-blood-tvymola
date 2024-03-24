9627ac2c47bb6a0f00a375adfe7fd588f324fa79
tvymola <156726450+tvymola@users.noreply.github.com>
2024-03-24 00:05:20 -0500

    Add another JS file for defining and calling a function

---
+++ index1.js
@@ -0,0 +1,32 @@
+// create an empty object named "person"
+const person = {};
+
+// set the "name" property of the "person" object
+person.name = "Tracy";
+
+// print the "person" object to the console
+console.log(person);
+
+// set the "age" property of the "person" object
+person.age = 22;
+
+// print the "person"  to object to the console
+console.log(person);
+
+// set the "hair color" property of the "person" object
+person.hairColor = "blonde";
+
+// print the "person" object to the console
+console.log(person);
+
+// set the "eye color" property of the "person" object
+person.eyeColor = "blue";
+
+// print the "person" object to the console
+console.log(person);
+
+// set the "occupation" property of the "person" object
+person.occupation = "E-Business Analyst";
+
+// print the "person" object to the console
+console.log(person);
