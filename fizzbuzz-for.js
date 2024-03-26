4c56141cead0fe9a466d3e71cff152b1f4e0d5a5
tvymola <156726450+tvymola@users.noreply.github.com>
2024-03-25 20:34:56 -0500

    Add comments to describe what the code is doing

--- fizzbuzz-for.js
+++ fizzbuzz-for.js
@@ -1,3 +1,9 @@
+/* Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+ multiples of 5 with "Buzz", and multiples of both 3 and 5 with "FizzBuzz". */
+
+// The number variable is initialized to 1
+// The for loop runs as long as the variable i is less than or equal to 100
+// The i variable is incremented by 1 in each iteration
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
--- fizzbuzz-while.js
+++ fizzbuzz-while.js
@@ -1,5 +1,10 @@
+/* Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+multiples of 5 with "Buzz", and multiples of both 3 and 5 with "FizzBuzz". */
+
+// The number variable is initialized to 1.
 let number = 1;
 
+// The while loop runs as long as the number is less than or equal to 100
 while (number <= 100) {
   if (number % 3 === 0 && number % 5 === 0) {
     console.log("FizzBuzz");
@@ -10,6 +15,6 @@ while (number <= 100) {
   } else {
     console.log(number);
   }
-
+  // The number variable is incremented by 1 in each iteration
   number++;
 }
