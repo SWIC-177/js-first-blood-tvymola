cef057ff2937814f77d59cdabc0d6df88d14b8bd
tvymola <156726450+tvymola@users.noreply.github.com>
2024-03-28 21:39:45 -0500

    Add the loop to process through the array for the second half of the homework, not quite working but getting closer

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const person = {
-  name: "John",
-  age: 17,
-};
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
 
 function verifyAdulthood(personName, personAge) {
   if (personAge >= 18) return `Welcome, ${personName}!`;
@@ -9,6 +27,8 @@ function verifyAdulthood(personName, personAge) {
   return `You are not old enough to enter, ${personName}.`;
 }
 
-const adultMessage = verifyAdulthood(person.name, person.age);
+for (let i = 0; i < people.length; i += 1) {
+  const adultMessage = verifyAdulthood(people[i].name, people[i].age);
 
-console.log(adultMessage);
+  console.log(adultMessage[i]);
+}
