cf114cd87de89cf36e7b1f6ca6ef09d87aba385c
tvymola <156726450+tvymola@users.noreply.github.com>
2024-03-29 16:44:14 -0500

    Track down the single detail that was preventing my code from running and add comments to describe the code

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/* This function takes a person's name and age as arguments and returns a message based on whether the person is age 18 or older. */
+
+// Function definition to produce a message based on the age of a person
+function verifyAdulthood(personName, personAge) {
+  if (personAge >= 18) return `Welcome, ${personName}!`;
+
+  return `You are not old enough to enter, ${personName}.`;
+}
+
+// An array of people objects with their name and age properties defined
 const people = [
   {
     name: "John",
@@ -21,14 +31,10 @@ const people = [
   },
 ];
 
-function verifyAdulthood(personName, personAge) {
-  if (personAge >= 18) return `Welcome, ${personName}!`;
-
-  return `You are not old enough to enter, ${personName}.`;
-}
-
+// This for loop will step through the array of people and call the verifyAdulthood function for each person in the people array
 for (let i = 0; i < people.length; i += 1) {
   const adultMessage = verifyAdulthood(people[i].name, people[i].age);
 
-  console.log(adultMessage[i]);
+  // Output the message to the console for every person in the people array
+  console.log(adultMessage);
 }
