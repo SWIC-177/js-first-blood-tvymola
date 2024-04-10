1d322b0de10b63c632e7b238a32cad0daa75fe4f
tvymola <156726450+tvymola@users.noreply.github.com>
2024-04-09 21:44:55 -0500

    Add pass by reference refactored to reduce code, WIP

---
+++ index4.js
@@ -0,0 +1,11 @@
+// Refactored to reduce the number of lines of code
+const person = { 
+	name: "John Doe",
+	age: 23,
+};
+function updatePersonName(p, newName) {
+	return { …p, name: newName, hobbies: ["reading", "writing"]};
+}
+
+const  updatedPerson = updatePersonName(person, "Jane Doe");
+console.log(updatedPerson);
